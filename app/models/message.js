ef5bc577a41c3f3aa7f6f6b981c2e9ac41288b65
muhammad muneer <muhammad.muneer91@gmail.com>
2015-03-10 17:22:58 +0200

    remove comments

--- app/models/message.js
+++ app/models/message.js
@@ -1,23 +1,3 @@
-// import {Model, Collection} from 'backbone';
-
-// class Person extends Model {
-//   defaults() {
-//     return {
-//       id: -1,
-//       name: '',
-//       description: 'Your average Joe'
-//     }
-//   }
-// }
-
-// class Persons extends Collection {
-//   model: Person
-// }
-
-// exports = {Person, Persons};
-
-// export default exports;
-
 var Backbone = require('backbone');
 
 var Message = Backbone.Model.extend({
